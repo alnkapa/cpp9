a72c50e62552f3fd3c9569da26889082b18a722e
Alexander A. Kapralov <alnkapa@gmail.com>
2024-10-11 18:42:00 +0300

    try

--- async/async.cpp
+++ async/async.cpp
@@ -1,54 +1,156 @@
 // library/async.cpp
 #include "async.h"
-#include <algorithm>
-#include <exception>
+#include "blocking_queue.h"
+#include <atomic>
 #include <iostream>
 #include <memory>
 #include <mutex>
 #include <stdexcept>
 #include <string>
-
-#include <condition_variable>
 #include <thread>
 #include <unordered_map>
-#include <vector>
+#include <utility>
+#include <variant>
 
-namespace async {
+namespace async
+{
 
-void run(context_type ctx, size_type N);
+class Worker
+{
+  private:
+    struct Done
+    {
+    };
+    std::thread m_thread;
+    size_type n;
+    std::atomic<bool> m_running{false};
+    BlockingQueue<std::variant<Done, std::string>> m_queue;
 
-static std::unordered_map<context_type, std::thread> ctx_map{};
-static std::mutex ctx_lock{};
+  public:
+    Worker(size_type N)
+        : n(N) {}
 
-context_type connect(size_type N) {
-  static size_type counter{0};
-  std::lock_guard lock(ctx_lock);
-  context_type ctx = ++counter;
-  ctx_map.emplace(ctx, std::thread(run, ctx, N));
-  return ctx;
+    ~Worker()
+    {
+        stop();
+    }
+
+    void
+    run()
+    {
+        if (!m_running.exchange(true)) // set true if false
+        {
+            m_thread = std::thread(&Worker::proccess, this);
+        }
+    }
+
+    void
+    stop()
+    {
+        if (m_running.exchange(false)) // set false if true
+        {
+            m_queue.add(Done{});
+        }
+
+        if (m_thread.joinable())
+        {
+            m_thread.join();
+        }
+    }
+
+    void
+    receive(const char_type *ptr, size_type size)
+    {
+        if (m_running)
+        {
+            m_queue.add(std::string{ptr, size});
+        }
+    }
+
+    void
+    proccess()
+    {
+        while (m_running)
+        {
+            auto val = m_queue.take();
+            if (std::holds_alternative<std::string>(val))
+            {
+                std::string message = std::get<std::string>(val);
+                std::cout << "message" << message << "\n";
+            }
+            else
+            {
+                break;
+            }
+        }
+        m_running = false;
+    }
 };
 
-void receive(context_type &ctx, const char_type *ptr, size_type size) {
-  std::unique_lock lock(ctx_lock);
-  const auto &it = ctx_map.find(ctx);
-  if (it == ctx_map.end()) {
-    throw std::runtime_error("Context not found");
-  }
-  context_type ctx_value = it->first;
-  lock.unlock();
-  // TODO: do with it
+class Publisher
+{
+  private:
+    std::unordered_map<context_type, std::shared_ptr<Worker>> m_subscribers;
+    std::mutex lo;
+
+  public:
+    void
+    subscribe(context_type &ctx, size_type N)
+    {
+        auto w = std::make_shared<Worker>(N);
+        {
+            std::lock_guard lock(lo);
+            m_subscribers.emplace(ctx, w);
+        }
+        w->run();
+    }
+
+    void
+    unsubscribe(context_type &ctx)
+    {
+        std::lock_guard lock(lo);
+        auto it = m_subscribers.find(ctx);
+        if (it == m_subscribers.end())
+        {
+            throw std::runtime_error("Context not found");
+        }
+        m_subscribers.erase(it);
+    }
+
+    void
+    notify(context_type &ctx, const char_type *ptr, size_type size)
+    {
+        std::lock_guard lock(lo);
+        auto it = m_subscribers.find(ctx);
+        if (it == m_subscribers.end())
+        {
+            throw std::runtime_error("Context not found");
+        }
+        it->second->receive(ptr, size);
+    }
 };
 
-void disconnect(context_type &ctx) {
-  std::lock_guard lock(ctx_lock);
-  const auto &it = ctx_map.find(ctx);
-  if (it == ctx_map.end()) {
-    throw std::runtime_error("Context not found");
-  }
-  if (it->second.joinable()) {
-    it->second.join();
-  }
-  ctx_map.erase(it);
-};
+static Publisher pub{};
+std::atomic<size_type> counter{0};
+
+context_type
+connect(size_type N)
+{
+    context_type ctx = ++counter;
+    pub.subscribe(ctx, N);
+    return ctx;
+}
+
+void
+receive(context_type &ctx, const char_type *ptr, size_type size)
+{
+    pub.notify(ctx, ptr, size);
+}
+
+void
+disconnect(context_type &ctx)
+{
+    pub.unsubscribe(ctx);
+}
 
 } // namespace async
--- main.cpp
+++ main.cpp
@@ -5,29 +5,36 @@
 #include <memory>
 #include <string>
 
-int main(int argc, char *argv[]) {
-  // if (argc != 2) {
-  //   std::cerr << "Usage: " << argv[0] << " N" << std::endl;
-  //   return 1;
-  // }
-  std::size_t N = 3;
-  try {
-    auto ptr =
-        std::unique_ptr<async::context_type, void (*)(async::context_type *)>(
-            new async::context_type(async::connect(N)),
-            [](async::context_type *p) {
-              async::disconnect(*p);
-              delete p;
-            });
+int
+main(int argc, char *argv[])
+{
+    // if (argc != 2) {
+    //   std::cerr << "Usage: " << argv[0] << " N" << std::endl;
+    //   return 1;
+    // }
+    std::size_t N = 3;
+    try
+    {
+        auto ptr =
+            std::unique_ptr<async::context_type, void (*)(async::context_type *)>(
+                new async::context_type(async::connect(N)),
+                [](async::context_type *p)
+                {
+                    async::disconnect(*p);
+                    delete p;
+                });
 
-    std::string command{};
-    while (std::getline(std::cin, command)) {
-      async::receive(*ptr, command.data(), command.size());
+        std::string command{};
+        while (std::getline(std::cin, command))
+        {
+            async::receive(*ptr, command.data(), command.size());
+        }
     }
-  } catch (const std::exception &err) {
-    std::cout << "error : " << err.what() << "\n";
-  }
-  return 0;
+    catch (const std::exception &err)
+    {
+        std::cout << "error : " << err.what() << "\n";
+    }
+    return 0;
 }
 
 // try {
